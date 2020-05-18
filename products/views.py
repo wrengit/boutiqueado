a3aed20be08c00741a103dc06d8b42863d091ce2
Lewis <ljleighfield@gmail.com>
2020-05-18 10:09:28 +0100

    add links to product queries

--- products/views.py
+++ products/views.py
@@ -1,15 +1,21 @@
 from django.shortcuts import render, get_object_or_404, reverse, redirect
 from django.contrib import messages
 from django.db.models import Q
-from .models import Product
+from .models import Product, Category
 
 
 def all_products(request):
     """A view to show all products"""
     products = Product.objects.all()
     query = None
+    categories = None
 
     if request.GET:
+        if "category" in request.GET:
+            categories = request.GET["category"].split(",")
+            products = products.filter(category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
+
         if "q" in request.GET:
             query = request.GET["q"]
             if not query:
@@ -18,7 +24,13 @@ def all_products(request):
 
             queries = Q(name__icontains=query) | Q(description__icontains=query)
             products = products.filter(queries)
-    context = {"products": products, "search_term": query}
+
+    context = {
+        "products": products,
+        "search_term": query,
+        "current_categories": categories,
+    }
+
     return render(request, "products/products.html", context)
 
 
